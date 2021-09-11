ed8b405ba9ff4174c130fe6aa2acad6cd2bec64f
Jager O <44758792+Baiterade@users.noreply.github.com>
2021-09-11 10:23:11 -0500

    Songkick Implementation v1

--- script.js
+++ script.js
@@ -1,69 +1,53 @@
+function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+  
+navigator.geolocation.getCurrentPosition(success);
+
+
 let map;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 11,
-    });
-  
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            const pos = {lat: position.coords.latitude, lng: position.coords.longitude};
-            map.setCenter(pos);
-        });
-    }
-
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 }
 
+document.getElementById("submit").addEventListener("click", getArtistData);
 
+function getArtistData() {
 
+  var artist = document.getElementById('artist-search').value;
 
+  var artistId;
 
+    fetch("https://api.songkick.com/api/3.0/search/artists.json?apikey=gKmSw1OM4IAL8F8j&query=" + artist)
+        .then(function (response) {
+            return response.json();
+            }
+        )
+        .then(function (data) {
+            console.log(data);
 
+            artistId = data.resultsPage.results.artist[0].id;
+            }
+        );   
 
+    fetch("https://api.songkick.com/api/3.0/artists/" + artistId + "/calendar.json?apikey=gKmSw1OM4IAL8F8j")
+  
+      .then(function (response) {
+        return response.json();
+       }
+        )
+      .then(function (data) {
+        console.log(data);
+        }
+  );  
 
-
-
-
-
-
-
-
-
-// var apiKey = 'WAITING ON SONGKICK :(';
-
-// var inputEl = document.getElementById('artist-search');
-// var artist = inputEl.value;
-
-// function getArtistId() {
-//     // event.preventDefault();
-
-//     // var citySearch = document.getElementById('city-search');
-//     // var city = citySearch.value;
-
-//     // if (savedCity) {
-//     //     city = savedCity
-//     // }
-    
-//     var queryUrl = "https://api.songkick.com/api/3.0/search/artists.json?apikey=" + apiKey + "&query=" + artist;
-      
-//     fetch(queryUrl)
-//         .then(function (response) {
-//             return response.json();
-//             }
-//         )
-//         .then(function (data) {
-//             artistId = data.resultsPage.results.artist.id;
-//             // searchedCity = data.name;
-//             getArtistData(artistId);              
-//             // getForecast();
-//             // storeCities(searchedCity);
-//             // getStoredCities(searchedCity);
-//             }
-//         );   
-// }
-// // function getArtistData(data) {
-
-// //     var queryUrl2 = https://api.songkick.com/api/3.0/artists/{artist_id}/calendar.json?apikey={your_api_key}
-    
-// //
+}
