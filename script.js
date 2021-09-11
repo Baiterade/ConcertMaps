355c9972286feb852e56618c5a1c0e512d143653
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-09-11 00:46:30 -0500

    Removed current location button in GM interface

--- script.js
+++ script.js
@@ -1,70 +1,19 @@
-// function success(pos) {
-//     var crd = pos.coords;
-  
-//     console.log('Your current position is:');
-//     console.log(`Latitude : ${crd.latitude}`);
-//     console.log(`Longitude: ${crd.longitude}`);
-//     console.log(`More or less ${crd.accuracy} meters.`);
-//   }
-  
-// //   function error(err) {
-// //     console.warn(`ERROR(${err.code}): ${err.message}`);
-// //   }
-  
-// navigator.geolocation.getCurrentPosition(success);
-// console.log(getCurrentPosition(pos));
-
-
-let map, infoWindow;
+let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 12,
-  });
-  infoWindow = new google.maps.InfoWindow();
-
-  const locationButton = document.createElement("button");
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 11,
+    });
+  
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-            // zoom: 21,
-          };
-
-        //   infoWindow.setPosition(pos);
-        //   infoWindow.setContent("Location found.");
-        //   infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, map.getCenter());
+        navigator.geolocation.getCurrentPosition((position) => {
+            const pos = {lat: position.coords.latitude, lng: position.coords.longitude};
+            map.setCenter(pos);
+        });
     }
-  });
-}
 
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//   infoWindow.setPosition(pos);
-//   infoWindow.setContent(
-//     browserHasGeolocation
-//       ? "Error: The Geolocation service failed."
-//       : "Error: Your browser doesn't support geolocation."
-//   );
-//   infoWindow.open(map);
-// }
+}
 
 
 
