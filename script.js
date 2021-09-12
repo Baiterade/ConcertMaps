dd227f273649bc5d276e92622da6c72a49806dcc
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-09-12 17:57:05 -0500

    Enabled info windows with event info, added military time conversion

--- script.js
+++ script.js
@@ -13,22 +13,25 @@ navigator.geolocation.getCurrentPosition(success);
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       const pos = {lat: position.coords.latitude, lng: position.coords.longitude};
-      map.setCenter(pos);
+
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: pos,
+        zoom: 8,
+      });
     })
   };
+  
 }
+
 
 document.getElementById("submit").addEventListener("click", getArtistData);
 
 var artistId;
+var concertInfo;
 
 function getArtistData() {
 
@@ -37,7 +40,7 @@ function getArtistData() {
   fetch("https://api.songkick.com/api/3.0/search/artists.json?apikey=gKmSw1OM4IAL8F8j&query=" + artist)
     .then(function (response) {
       return response.json();
-    }
+      }
     )
     .then(function (data) {
       console.log(data);
@@ -45,7 +48,7 @@ function getArtistData() {
       artistId = data.resultsPage.results.artist[0].id;
 
       getConcerts();
-    }
+      }
     );
 
 }
@@ -56,11 +59,75 @@ function getConcerts() {
 
     .then(function (response) {
       return response.json();
-    }
+      }
     )
     .then(function (data) {
       console.log(data);
-    }
+
+      concertInfo = data.resultsPage.results.event;
+
+      generateMarkers(concertInfo);
+      }
     );
+
+}
+
+function generateMarkers() {
+
+  var allMarkers = [];
+
+  for(i = 0; i < concertInfo.length; i++){
+    const marker = new google.maps.Marker({
+      position: {lat: concertInfo[i].venue.lat, lng: concertInfo[i].venue.lng},
+      map,
+    });
+
+    allMarkers.push(marker);
+
+    var milTime = concertInfo[i].start.time;
+
+    function convertMilTime() {
+      if (milTime === null) {
+        return '';
+      } else {
+        milTime.split(':');
+      }
+
+      var hour = (milTime.split(':'))[0];
+      var minute = (milTime.split(':'))[1];
+      
+      if ((milTime.split(':'))[0] === 12 || (milTime.split(':'))[0] <= 11) {
+        return hour + ':' + minute + ' A.M.'; 
+      } else {
+        var convHour = hour - 12;
+        return convHour + ':' + minute + ' P.M.'; 
+      }
+    }
+    
+    const concertString =
+      '<div id="content">' +
+      '<div id="siteNotice">' +
+      "</div>" +
+      '<h1 id="firstHeading" class="firstHeading">' + concertInfo[i].displayName + '</h1>' +
+      '<h3>' + concertInfo[i].location.city + '</h3>' +
+      '<h4>' + convertMilTime() + '</h4>' +
+      '<div id="bodyContent">' +
+      "<p></p>" +
+      '<p><a href="' + concertInfo[i].uri + '">' + "BUY TICKETS</a></p>" +
+      "</div>" +
+      "</div>";
+    
+    const infowindow = new google.maps.InfoWindow({
+      content: concertString,
+    });
+
+    allMarkers[i].addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+        shouldFocus: true,
+      });
+    });
+  }
 
 }
